48ba0413c02fc2aec5a8a00ca2d0d547aaf70e52
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-20 22:13:26 +0100

    first commmit

---
+++ src/components/main2/environments/environments.js
@@ -0,0 +1,10 @@
+import {React} from "react";
+
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const ENDPOINT_POPULAR = '/movie/popular';
+export const API_KEY = '?api_key=ebe0a7b19063d864de232de72766c4ee&language=en-US&page=';
+export const PAGE = '1';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w200';
+
+export default {BASE_URL, ENDPOINT_POPULAR, API_KEY, PAGE, IMAGE_BASE_URL};
