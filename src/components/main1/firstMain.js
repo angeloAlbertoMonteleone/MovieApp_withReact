7a1120c6d76e878d4a45a6c8cc3b93cc02fca810
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-20 23:14:57 +0100

    container, h3, <Card> created

--- src/components/main1/firstMain.js
+++ src/components/main1/firstMain.js
@@ -5,7 +5,7 @@ import {Container, Row, Col, Button} from 'react-bootstrap';
 const firstMain = () => {
     return (
         <Container className={styles.FirstMain}>
-            <Row class>
+            <Row className="">
                 <Col sm={12}>
                     <h1 className={styles.h1}>All Your Favorite Movies in the Same Place</h1>
                     <h3 className={styles.h3}>Check all the Categories, Personal Lists, Tv Shows, and free Streaming</h3>
