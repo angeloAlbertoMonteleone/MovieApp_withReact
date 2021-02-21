ebb3673a88097e9d231ee860e319472beed9e836
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-21 19:48:02 +0100

    first commit

---
+++ src/components/main3/scrolling/scrolling.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const scrolling = (props) => {
+        
+}
+
+
+export default scrolling;
---
+++ src/components/main3/thirdMain.js
@@ -0,0 +1,39 @@
+import  React, {useEffect, useState} from 'react';
+import styles from './thirdMain.module.css';
+import {Container, Row, Col} from 'react-bootstrap';
+import axios from 'axios';
+import {BASE_URL, ENDPOINT_POPULAR, API_KEY, PAGE, IMAGE_BASE_URL} from '../../environments/environments';
+import Card from './card/card';
+import Aux from '../../Auxiliary/Auxiliary';
+
+
+const SecondMain = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch(BASE_URL + ENDPOINT_POPULAR + API_KEY + PAGE).then((res) => res.json())
+        .then((data) => {
+         console.log(data);
+         setMovies(data.results)
+        });
+}, []);
+
+return (
+    <div className={styles.Container}>
+        <h3 className={styles.h3}>Most Popular</h3>
+        <div className={styles.movies}>
+            {movies.length > 0 && movies.map((movie) => {
+                return <div className={styles.movie}><Card className={styles.Card} data={movie} key={movie.id} {...movie}></Card></div> 
+            })}
+        </div>
+        
+        <div id="transparent_div_left"></div>
+        <div id="transparent_div_right"></div>
+    
+    </div>
+
+)
+
+}
+
+export default SecondMain;
