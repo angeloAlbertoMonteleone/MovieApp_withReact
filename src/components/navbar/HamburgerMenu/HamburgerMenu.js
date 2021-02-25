5243888cbd751a81adecdc93985759d654f1d594
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-25 21:44:47 +0100

    useState added for hamburger menu onclick

--- src/components/navbar/HamburgerMenu/HamburgerMenu.js
+++ src/components/navbar/HamburgerMenu/HamburgerMenu.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import classes from './hamburgerMenu.css';
+import React ,{useState} from 'react';
+import './HamburgerMenu.css';
+import requests from '../../../environments/environments';
+import Hamburger from './Hamburger/Hamburger';
+import Aux from "../../../Auxiliary/Auxiliary";
 
-const hamburgerMenu = (props) => {
-    return (
-        <div className={}></div>,
-        <div className={}></div>,
-        <div className={}></div>
-    )
+
+function HamburgerMenu() {
+    const [open, setOpen] = useState(false);
+
+        return (
+            <Aux>                   
+                <div className="Logo-ham" onClick={() => {setOpen(!open)}} >
+                    <div className="ham"></div>
+                    <div className="ham"></div>
+                    <div className="ham"></div>
+                </div>
+
+                <div className="hamburgerMenu" style={{transform: open ? "translateX(0px)" : "translateX(-500px)"}}>
+                    <Hamburger fetchUrl={requests.fetchGenres}></Hamburger>
+                </div>
+                <div className="body" style={{display: open ? "block" : "none"}} onClick={() => setOpen(false)}></div>
+            </Aux>
+        )
 }
 
-export default hamburgerMenu;
+export default HamburgerMenu;
