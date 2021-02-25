2e7844d669d6093021581b09fafb42779b7e3516
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-25 21:37:23 +0100

    first commit

---
+++ src/components/navbar/HamburgerMenu/Hamburger/Hamburger.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import axios from '../../../../axios/axios';
+import './Hamburger.css';
+import fire from '../../images/fire.jpg';
+
+function Hamburger({fetchUrl}) {
+    const [genres, setGenres] = useState([]);
+
+    useEffect(() => {
+        async function getCategories() {
+            const cat = await axios.get(fetchUrl);
+            console.log("cat:" ,cat.data.genres);
+            setGenres(cat.data.genres);
+            return cat;
+        }
+        getCategories();
+    }, [fetchUrl])
+
+    console.log('genres: ', genres)
+
+    return (
+        <div className="container_hamburger">
+            <img className="logo" src={fire}></img>
+            <h3>Categories</h3>
+            <ul className="ul-hamburger">
+                {genres.map((genre) => {
+                    return (
+                        <li className="li-hamburger" key={genre.id} id={genre.id} name={genre.name}>
+                            <a>{genre.name}</a></li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default Hamburger;
