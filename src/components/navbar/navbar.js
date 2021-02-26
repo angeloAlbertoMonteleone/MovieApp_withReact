18fb8dd51e07501dffed17bf7291648b4e48dad3
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-26 22:12:06 +0100

    NavDropdown and Search element created in Nav

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
+import {NavDropdown }from 'react-bootstrap';
 import './navbar.css';
 import fire from '../navbar/images/fire.jpg';
-import search from '../navbar/images/search.svg';
 import HamburgerMenu from '../navbar/HamburgerMenu/HamburgerMenu';
+import Search from './Search/Search';
 
 const Navbar = () => {
     const [show, getScroll] = useState(false);
@@ -20,8 +21,12 @@ const Navbar = () => {
     return (
         <div className={`Nav ${show && "nav_black"}`}>
             <HamburgerMenu></HamburgerMenu>
-            <img className="logo" src={fire}></img>
-            <img className="search" src={search}></img>
+            <img className="Logo" src={fire}></img>
+            <Search></Search>
+            <NavDropdown title="Settings" id="collasible-nav-dropdown" className="dropdown">
+                <NavDropdown.Item href="#action/3.1">Any help?</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Contact us!</NavDropdown.Item>
+            </NavDropdown>
         </div>
     )
 }
