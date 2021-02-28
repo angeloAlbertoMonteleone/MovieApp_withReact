1c5b786e17fc7da80b176f00a7fb4a1b6cd8f3b0
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-28 20:50:23 +0100

    useStates created to set query,movies, and movies rendered by movies.map

--- src/components/navbar/Search/Search.js
+++ src/components/navbar/Search/Search.js
@@ -1,52 +1,72 @@
 import React, {useState} from 'react';
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Container} from 'react-bootstrap';
 import axios from '../../../axios/axios';
 import search from '../images/search.svg';
 import './Search.css';
 import arrowLeft from '../images/arrow-left.svg';
 import Aux from '../../../Auxiliary/Auxiliary';
+const IMAGE_BASEURL = 'https://image.tmdb.org/t/p/w500/';
 
 function Search({fetchUrl}) {
     const [open, getOpen] = useState(false);
-    const [movies, getMovies] = useState([]);
-
+    /* create states input - movies */
+    const [query, setQuery] = useState('');
+    /* create the state for movies, and update the state appropriate */
+    const [movies, setMovies] = useState([]);
+            
             async function getMovie(e) {
-                const searchElement = e;
-                const mov = await axios.get(fetchUrl+ searchElement);
-                console.log("movie:" ,e);
+                e.preventDefault();
+                const mov = await axios.get(fetchUrl + query);
+                console.log("movie:", mov.data.results);
+                setMovies(mov.data.results)
                 return mov;
             }
-            getMovie();
         
     
     
     return (
         <Aux>
             <img className="search" src={search} onClick={() => {getOpen(true)}} style={{display : open ? "none" : "block"}}></img>
+            <Container fluid className={`search-menu ${open && "block"}`}>
+                    <Row>
+                        <Col className="Colomn">
+                            <div>
+                                <img className="back-arrow" src={arrowLeft} onClick={() => {
+                                    getOpen(false)
+                                    console.log('clicked')}}></img> 
+                                <form id="form" onSubmit={getMovie}>
+                                    <img className="search-inside-menu" src={search}></img>
+                                    <input type="text" name="searchEl" id="searchEl" placeholder="Search" value={query} onChange={(e) => setQuery(e.target.value)}></input>
+                                    {/* <button id="submit" type="submit">Search</button> */}
+                                </form>
+                            </div>
+                        </Col>
+                    </Row>
 
-            <div className={`search-menu ${open && "block"}`}>
-                <Row>
-                    <Col className="Colomn">
-                        <div>
-                            <img className="back-arrow" src={arrowLeft} onClick={() => {
-                                getOpen(false)
-                                console.log('clicked')}}></img> 
-                            <form id="form" 
-                                onClick={(e) => {
-                                getMovie(e.target);
-                                console.log('event',e.target);
-                                return false}}>
-                                <img className="search-inside-menu" src={search}></img>
-                                <input type="text" name="searchEl" id="searchEl" placeholder="search"></input>
-                                <button id="submit" type="submit">Search</button>
-                            </form>
-                        </div>
-                    </Col>
-                </Row>
-            </div>
+                    <div className="card-list">
+                        {movies && movies.length > 0 ? movies.map((movie) => {
+                            return (
+                            <Aux className="card-root">
+                                <img className="card-image"
+                                src={IMAGE_BASEURL + movie.poster_path}
+                                alt={movie.title + ' poster'}
+                                key={movie.id}></img>
 
+                                <div className="card-content" key={movie.id}>
+                                    <h3>{movie.title}</h3>
+                                    <p>RELEASE DATE: {movie.release_date}</p>
+                                    <p>RATING: {movie.vote_average}</p>
+                                    <p className="card-description">{movie.overview}</p>
+                                </div>
+                            </Aux>
+                        )}
+                        ): null}
+                    </div>
+            </Container>
             <div className="body-for-search" onClick={() => {getOpen(false)
             console.log('clicked by body')}} style={{display : open ? "block" : "none"}} ></div>
+
+            
         </Aux>
     )
 }
