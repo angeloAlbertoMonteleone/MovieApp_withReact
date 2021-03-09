c582330800afe51600ccbdeefcb0a4112d36b161
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-03-09 20:05:53 +0100

    new Youtube element created,handleClickForTrailer function created to get trailer by urlParams

--- src/components/row/row.js
+++ src/components/row/row.js
@@ -3,11 +3,12 @@ import axios from '../../axios/axios';
 import './row.css';
 import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
+import Aux from '../../Auxiliary/Auxiliary';
 const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 
 function Row({title, fetchUrl, smallerRow}) {
     const [movies, setMovies] = useState([]);
-    const [trailerUrl, setTrailerUrl] = useState('');
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     useEffect(() => {
         async function fetchData(){ 
@@ -29,40 +30,45 @@ function Row({title, fetchUrl, smallerRow}) {
         }
     };
 
-    // const handleClickForTrailer = (movie) => {
-    //     if(trailerUrl) {
-    //         setTrailerUrl('');
-    //     } else {
-    //         movieTrailer('Oceans Eleven', {id: true, multi:true})
-    //         .then(res => console.log(res));
-    //         // movieTrailer(movie?.name || '')
-    //         // .then((url) => {
-    //         //     const urlParameter = new URLSearchParams(new URL(url).search); 
-    //         //     setTrailerUrl(urlParameter.get('v'));
-    //         // })
-    //         // .catch(error => console.log(error));
-    //     }
-    // }
+    const handleClickForTrailer = (movie) => {
+        if(trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.title || "")
+            .then(url => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));  
+            }).
+            catch((error) => console.log(error));
+        }
+    }
 
 
     return (
-        <div className="row-movies">
-            {/* title */}
-            <h5>{title}</h5>
-            {/* posters */}
-            {/* add base url to poster path  */}
-            <div className="container_posters">
-                {movies && movies.length > 0 ? movies.map(movie => (
-                    <img
-                    // onClick={handleClickForTrailer(movie)} 
-                    className={`poster ${smallerRow && "smaller_row"}`} 
-                    src={`${BASE_IMAGE_URL}${movie.poster_path}`} 
-                    key={movie.id} 
-                    alt={movie.name}></img>))
-                : console.log('error: ',movies)}
+        <Aux>
+            <div className="row-movies">
+                {/* title */}
+                <h5>{title}</h5>
+                {/* posters */}
+                {/* add base url to poster path  */}
+                <div className="container_posters">
+                    {movies && movies.length > 0 ? movies.map(movie => (
+                        <img
+                        onClick={() => handleClickForTrailer(movie)} 
+                        className={`poster ${smallerRow && "smaller_row"}`} 
+                        src={`${BASE_IMAGE_URL}${movie.poster_path}`} 
+                        key={movie.id} 
+                        alt={movie.name}></img>))
+                    : console.log('error: ',movies)}
+                </div>
             </div>
-            <YouTube videoId={'cFAtUbi7a8w'} opts={opts}></YouTube>
-        </div>
+            
+            <div className="movieTrailer-container">
+                {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}></YouTube>}
+            </div>
+        
+            <div className="bodyTrailer"></div>
+        </Aux>
     ) 
 }
 
