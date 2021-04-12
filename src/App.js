3d829474590e8d8d87fd7147b483ae868863ee20
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-04-12 22:40:30 +0200

    Row has been change with MovieRow

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Component } from 'react';
+import { Component, useRef } from 'react';
 import './App.css';
 import Aux from '../src/Auxiliary/Auxiliary';
 import Nav from './components/navbar/navbar';
 import FirstMain from './components/main1/firstMain';
-import Row from './components/row/row';
-import requests from './environments/environments'
+import MovieRow from './components/row/row';
+import requests from './environments/environments';
 
 class App extends Component  {
   render () {
@@ -12,15 +12,15 @@ class App extends Component  {
       <Aux>
         <Nav></Nav>
         <FirstMain></FirstMain>
-        <Row title="Most Popular" fetchUrl={requests.fetchPopular}></Row>
-        <Row title="Comedies" fetchUrl={requests.fetchComedies}></Row>
-        <Row title="Upcoming Movies" fetchUrl={requests.fetchUpcoming}></Row>
-        <Row title="Family Movies" fetchUrl={requests.fetchFamilies}></Row>
-        <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
-        <Row title="The Originals" fetchUrl={requests.fetchOriginals} smallerRow></Row>
-        <Row title="Dramatic Movies" fetchUrl={requests.fetchDramas}></Row>
-        <Row title="Thrillers" fetchUrl={requests.fetchThrillers}></Row>
-        <Row title="Horrors" fetchUrl={requests.fetchHorrors}></Row>
+        <MovieRow title="Most Popular" fetchUrl={requests.fetchPopular}></MovieRow>
+        <MovieRow title="Comedies" fetchUrl={requests.fetchComedies}></MovieRow>
+        <MovieRow title="Upcoming Movies" fetchUrl={requests.fetchUpcoming}></MovieRow>
+        <MovieRow title="Family Movies" fetchUrl={requests.fetchFamilies}></MovieRow>
+        <MovieRow title="Top Rated" fetchUrl={requests.fetchTopRated}></MovieRow>
+        <MovieRow title="The Originals" fetchUrl={requests.fetchOriginals} biggerRow className ="biggerRow"></MovieRow>
+        <MovieRow title="Dramatic Movies" fetchUrl={requests.fetchDramas}></MovieRow>
+        <MovieRow title="Thrillers" fetchUrl={requests.fetchThrillers}></MovieRow>
+        <MovieRow title="Horrors" fetchUrl={requests.fetchHorrors}></MovieRow>
       </Aux>
     )
   }
