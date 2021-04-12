7edb68c00fe791b31f73cbe3b80cafc910392d2c
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-04-12 22:49:59 +0200

    new div created around ham-menu-logo

--- src/components/navbar/HamburgerMenu/HamburgerMenu.js
+++ src/components/navbar/HamburgerMenu/HamburgerMenu.js
@@ -9,17 +9,20 @@ function HamburgerMenu() {
     const [open, setOpen] = useState(false);
 
         return (
-            <Aux className="root">                   
-                <div className="Logo-ham" onClick={() => {setOpen(!open)}} >
-                    <div className="ham"></div>
-                    <div className="ham"></div>
-                    <div className="ham"></div>
-                </div>
+            <Aux className="root">
+                <div className="ham-menu" onClick={() => {setOpen(!open)}}>
+                    <div clicked className="Logo-ham">
+                        <div className="ham"></div>
+                        <div className="ham"></div>
+                        <div className="ham"></div>
+                    </div>
+                </div>                   
+                
 
                 <div className="hamburgerMenu" style={{transform: open ? "translateX(0px)" : "translateX(-500px)"}}>
-                    <Hamburger fetchUrl={requests.fetchGenres}></Hamburger>
+                    <Hamburger fetchUrl={requests.fetchGenres} fetchUrlMoviesTopRated={requests.fetchPopular}></Hamburger>
                 </div>
-                <div className="body" style={{display: open ? "block" : "none"}} onClick={() => setOpen(false)}></div>
+                <div className="body-next-HAM" style={{display: open ? "block" : "none"}} onClick={() => setOpen(false)}></div>
             </Aux>
         )
 }
